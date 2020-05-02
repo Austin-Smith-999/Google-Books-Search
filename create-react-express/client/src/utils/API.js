ae0db11513a752a8afb72a7380718fe75829ddeb
Austin Smith <austin.smith.ads@gmail.com>
2020-05-02 16:24:22 -0400

    API axios added

--- create-react-express/client/src/utils/API.js
+++ create-react-express/client/src/utils/API.js
@@ -1,1 +1,17 @@
 // axios calls from API
+import axios from "axios";
+
+const ApiUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+
+export default{
+    search: function (query) {
+    return axios.get(ApiUrl + query);    
+    },
+    getBooks: function () {
+        return axios.get("/api/save")
+    },
+    saveBooks: function (bookInfo) {
+        return axios.post("/api/save", bookInfo);
+    }
+}
+
