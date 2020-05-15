ed5c8ddbb2e1dd581470ffc9960d86f2a0a86aa2
Austin Smith <austin.smith.ads@gmail.com>
2020-05-14 23:20:21 -0400

    heroku34344

---
+++ client/build/precache-manifest.6e0dec79c77c6a5a47eb1002e76cd198.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "01a9ee00d0edab5cc3e0",
+    "url": "/static/js/main.2c6f71e8.chunk.js"
+  },
+  {
+    "revision": "e269cc1cdc267351c2a9",
+    "url": "/static/js/2.dccb2f18.chunk.js"
+  },
+  {
+    "revision": "01a9ee00d0edab5cc3e0",
+    "url": "/static/css/main.0fc24422.chunk.css"
+  },
+  {
+    "revision": "9609aee0caece62aeed816c1204c087d",
+    "url": "/index.html"
+  }
+];
